4bf698352458b3150625d5499b57ec95738b83a9
Wilson Hong <wilson400hong@gmail.com>
2026-07-17 10:28:25 -0700

    pretokenize utils + gitignore tmp

--- assignment1-basics/cs336_basics/bpe_tokenizer.py
+++ assignment1-basics/cs336_basics/bpe_tokenizer.py
@@ -1,29 +1,130 @@
-# TODO: incomplete
+import collections
+import multiprocessing
+import os
+import time
+from typing import BinaryIO, Collection
 
-"""
-Input
-input_path: str  Path to a text file with BPE tokenizer training data.
-vocab_size: int  A positive integer that defines the maximum final vocabulary size (including
-the initial byte vocabulary, vocabulary items produced from merging, and any special tokens).
-special_tokens: list[str]  A list of strings to add to the vocabulary. During training, treat
-them as hard boundaries that prevent merges across their spans, but do not include them when
-computing merge statistics.
-Your BPE training function should return the resulting vocabulary and merges:
-Output
-vocab: dict[int, bytes]  The tokenizer vocabulary, a mapping from int (token ID in the
-vocabulary) to bytes (token bytes).
-merges: list[tuple[bytes, bytes]]  A list of BPE merges produced from training. Each list
-item is a tuple of bytes (<token1>, <token2>), representing that <token1> was merged with
-<token2>. The merges should be ordered by order of creation.
-"""
+import regex as re
 
 
-class BPETokenizer:
-    def __init__(self):
-        pass
+def find_chunk_boundaries(
+    file: BinaryIO,
+    desired_num_chunks: int,
+    split_special_token: bytes,
+) -> list[int]:
+    """
+    Chunk the file into parts that can be counted independently.
+    May return fewer chunks if the boundaries end up overlapping.
+    """
+    assert isinstance(
+        split_special_token, bytes
+    ), "Must represent special token as a bytestring"
 
-    def encode(self):
-        pass
+    # Get total file size in bytes
+    file.seek(0, os.SEEK_END)
+    file_size = file.tell()
+    file.seek(0)
 
-    def decode(self):
-        pass
+    chunk_size = file_size // desired_num_chunks
+
+    # Initial guesses for chunk boundary locations, uniformly spaced
+    # Chunks start on previous index, don't include last index
+    chunk_boundaries = [i * chunk_size for i in range(desired_num_chunks + 1)]
+    chunk_boundaries[-1] = file_size
+
+    mini_chunk_size = 4096  # Read ahead by 4k bytes at a time
+
+    for bi in range(1, len(chunk_boundaries) - 1):
+        initial_position = chunk_boundaries[bi]
+        file.seek(initial_position)  # Start at boundary guess
+        while True:
+            mini_chunk = file.read(mini_chunk_size)  # Read a mini chunk
+
+            # If EOF, this boundary should be at the end of the file
+            if mini_chunk == b"":
+                chunk_boundaries[bi] = file_size
+                break
+
+            # Find the special token in the mini chunk
+            found_at = mini_chunk.find(split_special_token)
+            if found_at != -1:
+                chunk_boundaries[bi] = initial_position + found_at
+                break
+            initial_position += mini_chunk_size
+
+    # Make sure all boundaries are unique, but might be fewer than desired_num_chunks
+    return sorted(set(chunk_boundaries))
+
+
+def find_file_chunk_boundaries(
+    file_path: str,
+    desired_num_chunks: int,
+) -> list[int]:
+    with open(file_path, "rb") as f:
+        return find_chunk_boundaries(f, desired_num_chunks, b"<|endoftext|>")
+
+
+def pretokenize(file_path: str, start: int, end: int) -> dict[str, int]:
+    with open(file_path, "rb") as f:
+        f.seek(start)
+        chunk = f.read(end - start).decode("utf-8", errors="ignore")
+        PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+        counts = collections.Counter()
+        for m in re.finditer(PAT, chunk):
+            counts[m.group()] += 1
+        return dict(counts)
+
+
+def parallel_pretokenize(file_path: str, num_processes: int) -> dict[str, int]:
+    """
+    Pre-tokenize a file into chunks that can be counted independently.
+    """
+    t0 = time.perf_counter()
+    boundaries = find_file_chunk_boundaries(file_path, num_processes)
+    jobs = [
+        (file_path, start, end) for start, end in zip(boundaries[:-1], boundaries[1:])
+    ]
+    with multiprocessing.Pool(num_processes) as pool:
+        partial_counts = pool.starmap(pretokenize, jobs)
+
+    total_counts = collections.Counter()
+    for partial_count in partial_counts:
+        total_counts.update(partial_count)
+
+    t1 = time.perf_counter()
+    print(f"Elapsed: {t1 - t0}s")
+    print("First 10 Pretoken Counts:")
+    from itertools import islice
+
+    first_10 = dict(islice(total_counts.items(), 10))
+    for pretoken, count in first_10.items():
+        print(f"{pretoken!r}: {count}")
+
+    return dict(total_counts)
+
+
+BytesTuple = tuple[bytes, ...]
+BytesPair = tuple[bytes, bytes]
+
+
+def str_to_bt(s: str) -> BytesTuple:
+    return tuple(bytes([b]) for b in s.encode("utf-8"))
+
+
+def counts_to_bt(counts: dict[str, int]) -> dict[tuple[bytes], int]:
+    return {str_to_bt(k): v for k, v in counts.items()}
+
+
+def bt_to_bp(bt: BytesTuple) -> list[BytesPair]:
+    return list(zip(bt[:-1], bt[1:]))
+
+
+# class BPETokenizer:
+#     def __init__(self):
+#         pass
+
+#     def encode(self):
+#         pass
+
+#     def decode(self):
+#         pass
--- assignment1-basics/cs336_basics/pretokenization_example.py
+++ assignment1-basics/cs336_basics/pretokenization_example.py
@@ -56,61 +56,14 @@ def find_chunk_boundaries(
     return sorted(set(chunk_boundaries))
 
 
-def find_file_chunk_boundaries(
-    file_path: str,
-    num_processes: int,
-) -> list[int]:
-    with open(file_path, "rb") as f:
-        return find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
+## Usage
+with open(..., "rb") as f:
+    num_processes = 4
+    boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
 
-
-def pretokenize(file_path: str, start: int, end: int) -> dict[str, int]:
-    with open(file_path, "rb") as f:
+    # The following is a serial implementation, but you can parallelize this
+    # by sending each start/end pair to a set of processes.
+    for start, end in zip(boundaries[:-1], boundaries[1:]):
         f.seek(start)
         chunk = f.read(end - start).decode("utf-8", errors="ignore")
-        PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
-        counts = collections.Counter()
-        for m in re.finditer(PAT, chunk):
-            counts[m.group()] += 1
-        return dict(counts)
-
-
-def parallel_pretokenize(file_path: str, num_processes: int) -> dict[str, int]:
-    """
-    Pre-tokenize a file into chunks that can be counted independently.
-    """
-    t0 = time.perf_counter()
-    boundaries = find_file_chunk_boundaries(file_path, num_processes)
-    jobs = [
-        (file_path, start, end) for start, end in zip(boundaries[:-1], boundaries[1:])
-    ]
-    with multiprocessing.Pool(num_processes) as pool:
-        partial_counts = pool.starmap(pretokenize, jobs)
-
-    total_counts = collections.Counter()
-    for partial_count in partial_counts:
-        total_counts.update(partial_count)
-
-    t1 = time.perf_counter()
-    print(f"Elapsed: {t1 - t0}s")
-    print("First 10 Pretoken Counts:")
-    from itertools import islice
-
-    first_10 = dict(islice(total_counts.items(), 10))
-    for pretoken, count in first_10.items():
-        print(f"{pretoken!r}: {count}")
-
-    return dict(total_counts)
-
-
-## Usage
-# with open(..., "rb") as f:
-#     num_processes = 4
-#     boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
-
-#     # The following is a serial implementation, but you can parallelize this
-#     # by sending each start/end pair to a set of processes.
-#     for start, end in zip(boundaries[:-1], boundaries[1:]):
-#         f.seek(start)
-#         chunk = f.read(end - start).decode("utf-8", errors="ignore")
-#         # Run pre-tokenization on your chunk and store the counts for each pre-token
+        # Run pre-tokenization on your chunk and store the counts for each pre-token
