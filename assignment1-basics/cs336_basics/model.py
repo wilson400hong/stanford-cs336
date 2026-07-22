bc30280023d7be5ab52d6f2f5ca3700528082aad
Wilson Hong <wilson400hong@gmail.com>
2026-07-22 00:43:53 -0700

    RMSNorm

--- assignment1-basics/cs336_basics/model.py
+++ assignment1-basics/cs336_basics/model.py
@@ -14,7 +14,12 @@ class Linear(torch.nn.Module):
     ):
         super().__init__()
         self.W = torch.nn.Parameter(
-            torch.empty((out_features, in_features), dtype=dtype, device=device)
+            torch.empty(
+                (out_features, in_features),
+                dtype=dtype,
+                device=device,
+                requires_grad=True,
+            )
         )
         var = 2 / (in_features + out_features)
         std = math.sqrt(var)
@@ -35,9 +40,36 @@ class Embedding(torch.nn.Module):
     ):
         super().__init__()
         self.embeddings = torch.nn.Parameter(
-            torch.empty((num_embeddings, embedding_dim), dtype=dtype, device=device)
+            torch.empty(
+                (num_embeddings, embedding_dim),
+                dtype=dtype,
+                device=device,
+                requires_grad=True,
+            )
         )
         torch.nn.init.trunc_normal_(self.embeddings, std=1.0, a=-3.0, b=3.0)
 
     def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
         return self.embeddings[token_ids]
+
+
+class RMSNorm(torch.nn.Module):
+    def __init__(
+        self,
+        d_model: int,
+        eps: float = 1e-5,
+        device: torch.device | None = None,
+        dtype: torch.dtype | None = None,
+    ):
+        super().__init__()
+        self.eps = eps
+        self.G = torch.nn.Parameter(
+            torch.ones(d_model, dtype=dtype, device=device), requires_grad=True
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        in_dtype = x.dtype
+        x = x.to(torch.float32)
+        denom = torch.sqrt(torch.mean(torch.pow(x, 2), dim=-1, keepdim=True) + self.eps)
+        res = (x * self.G) / denom
+        return res.to(in_dtype)
--- assignment1-basics/tests/adapters.py
+++ assignment1-basics/tests/adapters.py
@@ -7,7 +7,7 @@ from typing import Any, BinaryIO, IO
 import numpy.typing as npt
 import torch
 from cs336_basics.bpe import BPETokenizer, train_bpe
-from cs336_basics.model import Embedding, Linear
+from cs336_basics.model import Embedding, Linear, RMSNorm
 from jaxtyping import Bool, Float, Int
 from torch import Tensor
 
@@ -387,7 +387,9 @@ def run_rmsnorm(
         Float[Tensor,"... d_model"]: Tensor of with the same shape as `in_features` with the output of running
         RMSNorm of the `in_features`.
     """
-    raise NotImplementedError
+    rmsnorm = RMSNorm(d_model, eps)
+    rmsnorm.load_state_dict({"G": weights})
+    return rmsnorm(in_features)
 
 
 def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
