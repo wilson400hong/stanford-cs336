f1c72a9e6b7527a61eb18a2b9aa65c9fce66e38b
Wilson Hong <wilson400hong@gmail.com>
2026-07-22 20:34:39 -0700

    transformer lm

--- assignment1-basics/cs336_basics/model.py
+++ assignment1-basics/cs336_basics/model.py
@@ -292,9 +292,54 @@ class TransformerBlock(torch.nn.Module):
         x: Float[Tensor, "batch sequence_length d_model"],
         token_positions: Int[Tensor, " ... sequence_length"] | None = None,
     ) -> torch.Tensor:
-        # TODO: token_positions!!
         y = x + self.attn(self.ln1(x), token_positions)
         return y + self.ffn(self.ln2(y))
+
+
+class TransformerLM(torch.nn.Module):
+    def __init__(
+        self,
+        vocab_size: int,
+        context_length: int,
+        d_model: int,
+        num_layers: int,
+        num_heads: int,
+        d_ff: int,
+        rope_theta: float,
+        eps: float = 1e-5,
+        device: torch.device | None = None,
+        dtype: torch.dtype | None = None,
+    ):
+        super().__init__()
+        self.token_embeddings = Embedding(vocab_size, d_model, device, dtype)
+        self.layers = torch.nn.ModuleList(
+            [
+                TransformerBlock(
+                    d_model,
+                    num_heads,
+                    d_ff,
+                    context_length,
+                    rope_theta,
+                    eps,
+                    device,
+                    dtype,
+                )
+                for _ in range(num_layers)
+            ]
+        )
+        self.ln_final = RMSNorm(d_model, eps, device, dtype)
+        self.lm_head = Linear(d_model, vocab_size, device, dtype)
+
+    def forward(
+        self, in_indices: Int[Tensor, " batch_size sequence_length"]
+    ) -> torch.Tensor:
+        seq_len = in_indices.shape[-1]
+        token_positions = torch.arange(seq_len, device=in_indices.device)
+        x = self.token_embeddings(in_indices)
+        for transformer in self.layers:
+            x = transformer(x, token_positions)
+        y = self.ln_final(x)
+        return self.lm_head(y)
 
 
 # class ModernRotaryPositionalEmbedding(torch.nn.Module):
--- assignment1-basics/tests/adapters.py
+++ assignment1-basics/tests/adapters.py
@@ -18,6 +18,7 @@ from cs336_basics.model import (
     softmax,
     SwiGLU,
     TransformerBlock,
+    TransformerLM,
 )
 from jaxtyping import Bool, Float, Int
 from torch import Tensor
@@ -404,7 +405,11 @@ def run_transformer_lm(
         Float[Tensor, "batch_size sequence_length vocab_size"]: Tensor with the predicted unnormalized
         next-word distribution for each token.
     """
-    raise NotImplementedError
+    llm = TransformerLM(
+        vocab_size, context_length, d_model, num_layers, num_heads, d_ff, rope_theta
+    )
+    llm.load_state_dict(weights)
+    return llm(in_indices)
 
 
 def run_rmsnorm(
